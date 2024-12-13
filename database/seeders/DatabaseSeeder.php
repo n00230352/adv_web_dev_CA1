e727eab488d97a5c93cf3111394d503ce7754b19
giorgia <n00230352@iadt.ie>
2024-12-13 11:13:00 +0000

    Database seeder fix

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call(ItemSeeder::class);
         $this->call(CategorySeeder::class);
+        $this->call(ItemSeeder::class);
     }
 }
